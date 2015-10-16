dca454bfc190880e5cd24328282ab3a0355fb24b
nosound-nowork <nosound.nowork@gmail.com>
2015-10-16 17:29:56 +0900

    modified js

--- js/test_2.js
+++ js/test_2.js
@@ -3,7 +3,7 @@
 	$(function () {
 		
 		var pages = new Pages("div.contents"),
-			timer = new Timer(30),
+			timer = new Timer(),
 			timerId = null,
 			time = $("div.time"),
 			count = { total: 0, correct: 0 };
@@ -53,34 +53,41 @@
 			$.getJSON("../test/q.json", function (data) {
 				
 				var dice = random(data.dice.questions, 3),
-					calc = [],
-					image = [],
+					image = random(data.image, 5),
+					calc = random(data.calc, 5),
 					ePage = $("div.page_e");
 				
-				$.each(dice, function (index, value) {
+				$.each(dice, function (index, value) { ePage.before(makePageQ("dice", value)); });
+				
+				$.each(image, function (index, value) { ePage.before(makePageQ("image", value[0])); });
+				
+				$.each(calc, function (index, value) { ePage.before(makePageQ("calc", value[0])); });
+				
+				setTimeout(function () { time.show(); pages.next(); }, 1500);
+				
+				function makePageQ(type, value) {
 					
-					var table = $(
-						"<table><tbody>" +
-							"<tr><td></td><td></td><td></td></tr>" +
-							"<tr><td></td><td></td><td></td></tr>" +
-							"<tr><td></td><td></td><td></td></tr>" +
-							"<tr><td></td><td></td><td></td></tr>" +
-						"</tbody></table>"
-					);
+					var pageQ = $("<div>").addClass("page_q");
 					
-					var cells = table.find("td");
-					
-					value = String(value);
-					
-					for (var i = 0; i < value.length; i++) {
+					if (type == "dice") {
 						
-						var char = value.charAt(i);
+						var table = $(
+							"<table><tbody>" +
+								"<tr><td></td><td></td><td></td></tr>" + "<tr><td></td><td></td><td></td></tr>" +
+								"<tr><td></td><td></td><td></td></tr>" + "<tr><td></td><td></td><td></td></tr>" +
+							"</tbody></table>"
+						);
 						
-						$(cells[i])
-							.addClass("q_" + char)
-							.data("char", char)
-							.append($("<span>").text("Q"))
-							.click(function () {
+						var cells = table.find("td");
+						
+						value = String(value);
+						
+						for (var i = 0; i < value.length; i++) {
+							
+							var char = value.charAt(i),
+								cell = $(cells[i]).addClass("q_" + char).data("char", char);
+							
+							cell.click(function () {
 								
 								var c = $(this).data("char");
 								
@@ -93,21 +100,44 @@
 									pages.next();
 								}
 							});
+						}
+						
+						pageQ.append($("<h3>").append($("<span>").text(data.dice.description))).append(table);
+						
+					} else {
+						
+						pageQ.append($("<h3>").append($("<span>").text(value.question))).append(list);
+						
+						if (typeof value.file !== "undefined") {
+							
+							pageQ.append($("<img>").attr("src", "../img/q/" + value.file));
+						}
+						
+						var list = $("<ol>");
+						
+						$.each(value.selections, function (j, val) {
+							
+							var answer = $("<li>").text(val);
+							
+							if (value.answer == j) answer.addClass("correct");
+							
+							answer.click(function () {
+								
+								count.total++;
+								
+								if ($(this).hasClass("correct")) count.correct++;
+								
+								pages.next();
+							});
+							
+							list.append(answer);
+						});
+						
+						pageQ.append(list);
 					}
 					
-					ePage.before(
-						$("<div>")
-							.addClass("page_q")
-							.append(
-								$("<h3>").append(
-									$("<span>").text(data.dice.description)
-								)
-							)
-							.append(table)
-					);
-				});
-				
-				setTimeout(function () { time.show(); pages.next(); }, 1500);
+					return pageQ;
+				}
 			});
 		}
 		
@@ -121,7 +151,7 @@
 				
 				ret.push(ary.splice(Math.floor(Math.random() * ary.length), 1));
 				
-				n--
+				n--;
 			}
 			
 			return ret;
