c603b753fad27096ac6582b4f8314d5a52e1d6a6
nosound-nowork <nosound.nowork@gmail.com>
2015-10-20 13:07:43 +0900

    modified mailto

--- js/record.js
+++ js/record.js
@@ -77,18 +77,20 @@
 					encodeURIComponent("［年齢］") + br() + sp(4) +
 					encodeURIComponent($("#age > option:selected").text()) + br() + sp(1) + br() +
 					encodeURIComponent("［○○○○テスト］") +  br() +
-					sp(8) + encodeURIComponent("音なし：") + $("#test1_off").parent().text() +
-					sp(1) + encodeURIComponent("／") + sp(1) +
-					encodeURIComponent("音あり：") + $("#test1_on").parent().text() +  br() +
+					sp(8) + encodeURIComponent("音なし：") + $("#test1_off").parent().text() + br() +
+					sp(8) + encodeURIComponent("音あり：") + $("#test1_on").parent().text() + br() +
 					encodeURIComponent("［××××テスト］")  +  br() +
 					sp(4) + encodeURIComponent("テンポ - 遅い") +  br() +
 					sp(8) + encodeURIComponent("最高回答数：") +
-					$("#test2_slow_total_total").parent().parent().text() + sp(1) + encodeURIComponent("／") + sp(1) +
-					encodeURIComponent("最高正解率：") + $("#test2_slow_rate_total").parent().parent().text() +  br() +
+					$("#test2_slow_total_total").parent().parent().text() + br() +
+					sp(8) + encodeURIComponent("最高正解率：") +
+					$("#test2_slow_rate_total").parent().parent().text() +  br() +
 					sp(4) + encodeURIComponent("テンポ - 早い") +  br() +
 					sp(8) + encodeURIComponent("最高回答数：") +
-					$("#test2_fast_total_total").parent().parent().text() + sp(1) + encodeURIComponent("／") + sp(1) +
-					encodeURIComponent("最高正解率：") + $("#test2_fast_rate_total").parent().parent().text()
+					$("#test2_fast_total_total").parent().parent().text() + br() +
+					sp(8) + encodeURIComponent("最高正解率：") +
+					$("#test2_fast_rate_total").parent().parent().text() + br() + br() +
+					encodeURIComponent("［その他 感想など］") + br() + sp(1) + br()
 			);
 		}
 		
