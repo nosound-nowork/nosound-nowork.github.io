37ef2b42c94a20ddb2b4eecf5c6b7c4b1f817c95
nosound-nowork <nosound.nowork@gmail.com>
2016-01-06 16:11:16 +0900

    Close

--- js/record.js
+++ js/record.js
@@ -61,26 +61,40 @@
 		
 		mailto();
 		
+//		= = = = = = = = = = =
+//		 2016/01/06  CLOSE
+//		= = = = = = = = = = =
+		$("#age").attr("disabled", true);
+		
+		$("#mailto").parent().addClass("disabled");
+		
+		$("div.policy").html(
+			"記録のご報告は、<br>2015年12月末日で締め切りました。<br><br>ご協力ありがとうございました。"
+		);
+		
 		function mailto() {
 			
-			$("#mailto").attr("href",
-				"mailto:" + "nosound.nowork" + "@" + "gmail.com?" +
-				"subject=" + ec("ユニシス研究会：静岡G 記録報告") + "&" +
-				"body=" +
-					ec("［年齢］") + br(1) + sp(4) +
-					ec($("#age > option:selected").text()) + br(2) +
-					ec("［Quick Touch］") +  br(1) +
-					sp(4) + ec("テンポ - 遅い：") + $("#test1_slow").parent().text() + br(1) +
-					sp(4) + ec("テンポ - 速い：") + $("#test1_fast").parent().text() + br(2) +
-					ec("［Rapid Answer］")  +  br(1) +
-					sp(4) + ec("音なし") +  br(1) +
-					sp(8) + ec("最高回答数：") + $("#test2_off_total_total").parents("dd").text() + br(1) +
-					sp(8) + ec("最高正解率：") + $("#test2_off_rate_total").parents("dd").text() +  br(1) +
-					sp(4) + ec("音あり") +  br(1) +
-					sp(8) + ec("最高回答数：") + $("#test2_on_total_total").parents("dd").text() + br(1) +
-					sp(8) + ec("最高正解率：") + $("#test2_on_rate_total").parents("dd").text() + br(2) +
-					ec("［その他 感想など］") + br(1)
-			);
+//			= = = = = = = = = = =
+//			 2016/01/06  CLOSE
+//			= = = = = = = = = = =
+//			$("#mailto").attr("href",
+//				"mailto:" + "nosound.nowork" + "@" + "gmail.com?" +
+//				"subject=" + ec("ユニシス研究会：静岡G 記録報告") + "&" +
+//				"body=" +
+//					ec("［年齢］") + br(1) + sp(4) +
+//					ec($("#age > option:selected").text()) + br(2) +
+//					ec("［Quick Touch］") +  br(1) +
+//					sp(4) + ec("テンポ - 遅い：") + $("#test1_slow").parent().text() + br(1) +
+//					sp(4) + ec("テンポ - 速い：") + $("#test1_fast").parent().text() + br(2) +
+//					ec("［Rapid Answer］")  +  br(1) +
+//					sp(4) + ec("音なし") +  br(1) +
+//					sp(8) + ec("最高回答数：") + $("#test2_off_total_total").parents("dd").text() + br(1) +
+//					sp(8) + ec("最高正解率：") + $("#test2_off_rate_total").parents("dd").text() +  br(1) +
+//					sp(4) + ec("音あり") +  br(1) +
+//					sp(8) + ec("最高回答数：") + $("#test2_on_total_total").parents("dd").text() + br(1) +
+//					sp(8) + ec("最高正解率：") + $("#test2_on_rate_total").parents("dd").text() + br(2) +
+//					ec("［その他 感想など］") + br(1)
+//			);
 		}
 		
 		function ec(s) { return encodeURIComponent(s); }
